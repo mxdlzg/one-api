5b807f08f0e8ec0764ba6071a76e979f2ce021cd
Qiying Wang <781345688@qq.com>
2024-07-06 13:19:41 +0800

    feat: refactor AwsClaude to Aws to support both llama3 and claude (#1601)
    * feat: refactor AwsClaude to Aws to support both llama3 and claude
    
    * fix: aws llama3 ratio

--- relay/controller/helper.go
+++ relay/controller/helper.go
@@ -96,7 +96,7 @@ func postConsumeQuota(ctx context.Context, usage *relaymodel.Usage, meta *meta.M
 		return
 	}
 	var quota int64
-	completionRatio := billingratio.GetCompletionRatio(textRequest.Model)
+	completionRatio := billingratio.GetCompletionRatio(textRequest.Model, meta.ChannelType)
 	promptTokens := usage.PromptTokens
 	completionTokens := usage.CompletionTokens
 	quota = int64(math.Ceil((float64(promptTokens) + float64(completionTokens)*completionRatio) * ratio))
